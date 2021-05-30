bebb8e148b88681a33bd16b05f7ee9743d64b083
Elhadi LEKMITI <elhadi.lekmiti@zsoft-consulting.com>
2021-05-30 15:15:57 +0200

    Account is an ddd entity: override equals and hashcode

--- src/main/kotlin/domain/bankaccount/Account.kt
+++ src/main/kotlin/domain/bankaccount/Account.kt
@@ -12,11 +12,17 @@ class Account(
 ) {
 
     init {
-        check("""^(?:(?:IT|SM)\d{2}[A-Z]\d{22}|CY\d{2}[A-Z]\d{23}|NL\d{2}[A-Z]{4}\d{10}|LV\d{2}[A-Z]{4}\d{13}|(?:BG|BH|GB|IE)\d{2}[A-Z]{4}\d{14}|GI\d{2}[A-Z]{4}\d{15}|RO\d{2}[A-Z]{4}\d{16}|KW\d{2}[A-Z]{4}\d{22}|MT\d{2}[A-Z]{4}\d{23}|NO\d{13}|(?:DK|FI|GL|FO)\d{16}|MK\d{17}|(?:AT|EE|KZ|LU|XK)\d{18}|(?:BA|HR|LI|CH|CR)\d{19}|(?:GE|DE|LT|ME|RS)\d{20}|IL\d{21}|(?:AD|CZ|ES|MD|SA)\d{22}|PT\d{23}|(?:BE|IS)\d{24}|(?:FR|MR|MC)\d{25}|(?:AL|DO|LB|PL)\d{26}|(?:AZ|HU)\d{27}|(?:GR|MU)\d{28})${'$'}""".toRegex().matches(iban)) { "invalid iban $iban" }
+        check(
+            """^(?:(?:IT|SM)\d{2}[A-Z]\d{22}|CY\d{2}[A-Z]\d{23}|NL\d{2}[A-Z]{4}\d{10}|LV\d{2}[A-Z]{4}\d{13}|(?:BG|BH|GB|IE)\d{2}[A-Z]{4}\d{14}|GI\d{2}[A-Z]{4}\d{15}|RO\d{2}[A-Z]{4}\d{16}|KW\d{2}[A-Z]{4}\d{22}|MT\d{2}[A-Z]{4}\d{23}|NO\d{13}|(?:DK|FI|GL|FO)\d{16}|MK\d{17}|(?:AT|EE|KZ|LU|XK)\d{18}|(?:BA|HR|LI|CH|CR)\d{19}|(?:GE|DE|LT|ME|RS)\d{20}|IL\d{21}|(?:AD|CZ|ES|MD|SA)\d{22}|PT\d{23}|(?:BE|IS)\d{24}|(?:FR|MR|MC)\d{25}|(?:AL|DO|LB|PL)\d{26}|(?:AZ|HU)\d{27}|(?:GR|MU)\d{28})${'$'}""".toRegex()
+                .matches(iban)
+        ) { "invalid iban $iban" }
     }
 
     fun deposit(amount: MoneyAmount): Balance {
-        validateCurrencies(currency, amount.currency) { "${amount.currency} unmatched with account currency $currency " }
+        validateCurrencies(
+            currency,
+            amount.currency
+        ) { "${amount.currency} unmatched with account currency $currency " }
         val depositTransaction = aDeposit(amount = amount, tempBalance = balance.plus(amount))
         transactions = transactions + depositTransaction
         balance = depositTransaction.tempBalance
@@ -24,7 +30,10 @@ class Account(
     }
 
     fun withdraw(amount: MoneyAmount): Balance {
-        validateCurrencies(currency, amount.currency) { "${amount.currency} unmatched with account currency $currency " }
+        validateCurrencies(
+            currency,
+            amount.currency
+        ) { "${amount.currency} unmatched with account currency $currency " }
         val withdrawTransaction = aWithdraw(amount = amount, tempBalance = balance.minus(amount))
         transactions = transactions + withdrawTransaction
         balance = withdrawTransaction.tempBalance
@@ -33,6 +42,17 @@ class Account(
 
     fun printUsing(printingStrategy: PrintingStrategy): String =
         printingStrategy.print(transactions)
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+        other as Account
+        return iban == other.iban
+    }
+
+    override fun hashCode(): Int = iban.hashCode()
+
+
 }
 
 typealias Iban = String
